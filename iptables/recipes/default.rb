d21cedd058d07cadb18c8baba8ea90e673b370ac
Jay Marcyes <jay@marcyes.com>
2013-11-15 17:55:54 -0800

    iptables will now only do anything if there is a port or a whitelist

--- iptables/recipes/default.rb
+++ iptables/recipes/default.rb
@@ -1,34 +1,38 @@
 name = cookbook_name.to_s
 n = node[name]
 
-service_name = "iptables-config"
-config = ::File.join("", "opt", "#{service_name}.sh")
-template config do
-  source "iptables.sh.erb"
-  owner "root"
-  group "root"
-  mode "0700"
-  variables(
-    "open_ports" => n['open_ports'],
-    "whitelist" => n["whitelist"],
-  )
-  notifies :start, "service[#{service_name}]", :delayed
-end
+# you can really bite yourself in the foot here if you haven't opened at least ssh
+if !n['open_ports'].empty? or !n['white_list'].empty?
 
-template ::File.join("", "etc", "init", "#{service_name}.conf") do
-  source "iptables.conf.erb"
-  owner "root"
-  group "root"
-  mode "0655"
-  variables(
-    "start_script" => config
-  )
-  notifies :start, "service[#{service_name}]", :delayed
-end
+  service_name = "iptables-config"
+  config = ::File.join("", "opt", "#{service_name}.sh")
+  template config do
+    source "iptables.sh.erb"
+    owner "root"
+    group "root"
+    mode "0700"
+    variables(
+      "open_ports" => n['open_ports'],
+      "whitelist" => n["whitelist"],
+    )
+    notifies :start, "service[#{service_name}]", :delayed
+  end
 
-service service_name do
-  provider Chef::Provider::Service::Upstart
-  action :nothing
-  supports :start => true, :stop => true, :status => false, :restart => false
-end
+  template ::File.join("", "etc", "init", "#{service_name}.conf") do
+    source "iptables.conf.erb"
+    owner "root"
+    group "root"
+    mode "0655"
+    variables(
+      "start_script" => config
+    )
+    notifies :start, "service[#{service_name}]", :delayed
+  end
 
+  service service_name do
+    provider Chef::Provider::Service::Upstart
+    action :nothing
+    supports :start => true, :stop => true, :status => false, :restart => false
+  end
+
+end
