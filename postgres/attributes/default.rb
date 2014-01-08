0838944b9ef062f8910afd09cce5c7fe06745cba
Jay Marcyes <jay@marcyes.com>
2014-01-08 13:30:50 -0800

    just realized I had borked postgres recipe, not fixed it, but it is now fixed

--- postgres/attributes/default.rb
+++ postgres/attributes/default.rb
@@ -7,14 +7,11 @@ default[name]["users"] = {"postgres" => "postgres"}
 # the databases hash is in username => [dbname1, ...] format
 default[name]["databases"] = {}
 
-version_str = %x(dpkg -s postgresql|grep Version)
+version_str = %x(apt-cache show postgresql|grep Version)
 m = version_str.match(/^Version:\s*([\d\.]+)/i)
-if m
-  version = m[1]
-
-  default[name]["version"] = version
-  default[name]["conf_file"] = ::File.join("", "etc", "postgresql", version, "main", "postgresql.conf")
-end
+version = m[1]
+default[name]["version"] = version
+default[name]["conf_file"] = ::File.join("", "etc", "postgresql", version, "main", "postgresql.conf")
 
 default[name]["conf"] = {}
 default[name]["conf"]["listen_addresses"] = "'*'"
