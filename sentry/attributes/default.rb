470b0dd935c3497d2d66d070ef33ab3b7f6084a6
Jay Marcyes <jay@marcyes.com>
2014-01-31 15:28:33 -0800

    removes sentry from the dev and test environments. Adds newrelic

--- sentry/attributes/default.rb
+++ sentry/attributes/default.rb
@@ -7,18 +7,3 @@ default[name] = {}
 default[name]["db"] = "postgres"
 default[name]["user"] = "www-data"
 
-# default[name]['services'] = {
-#   'nsqlookupd' => {
-#     'command' => 'nsqlookupd --http-address=0.0.0.0:4161 --tcp-address=0.0.0.0:4160',
-#     'action' => :start,
-#   },
-#   'nsqd' => {
-#     'command' => "nsqd --lookupd-tcp-address=127.0.0.1:4160 --data-path=#{default[name]["data_dir"]}",
-#     'action' => :start,
-#   },
-#   'nsqadmin' => {
-#     'command' => "nsqadmin --http-address=0.0.0.0:4171 --lookupd-http-address=127.0.0.1:4161 --template-dir=#{::File.join(default[name]["share_dir"], 'nsqadmin', 'templates')}",
-#     'action' => :start,
-#   },
-# }
-# 
--- sentry/metadata.rb
+++ sentry/metadata.rb
@@ -1,14 +1,13 @@
 # http://docs.opscode.com/essentials_cookbook_metadata.html
-name              "nsq"
+name              "sentry"
 maintainer        "Jay Marcyes"
 maintainer_email  "jay@marcyes.com"
-description       "install and configure nsq for message passing goodness"
-version           "0.2"
+description       "install sentry"
+version           "0.1"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 supports          "ubuntu", "12.04"
 
 depends           "pip"
 
 recipe            "sentry", "install sentry server"
-recipe            "sentry::python", "install raven python sentry bindings"
 
