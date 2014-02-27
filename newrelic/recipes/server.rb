bdd1d5cbbeb4aa8c00c811608b482512c332a637
Jay Marcyes <jay@marcyes.com>
2014-02-26 16:47:50 -0800

    typo

--- newrelic/recipes/server.rb
+++ newrelic/recipes/server.rb
@@ -5,13 +5,13 @@ n = node[name]
 nr_deb_file = ::File.join("", "etc", "sources.list.d", "newrelic.list")
 execute "echo deb http://apt.newrelic.com/debian/ newrelic non-free >> #{nr_deb_file}" do
   not_if "test -f #{nr_deb_file}"
-  notifies, :run, "execute[nr add gpg key]", :immediately
+  notifies :run, "execute[nr add gpg key]", :immediately
 end
 
 execute "nr add gpg key" do
   command "wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -"
   action :none
-  notifies, :run, "execute[nr apt update]", :immediately
+  notifies :run, "execute[nr apt update]", :immediately
 end
 
 execute "nr apt update" do
