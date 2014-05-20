4d689909910acc08496f76f823bc7fc5defad11a
Jay Marcyes <jay@marcyes.com>
2014-05-20 16:14:57 -0700

    and more fixes

--- mongrel2/recipes/python.rb
+++ mongrel2/recipes/python.rb
@@ -4,14 +4,14 @@ n = node[name]
 include_recipe "zeromq::python"
 include_recipe "#{name}::src"
 
+src_dir = ::File.join(::Chef::Config[:file_cache_path], name, n["version"])
+
 package "python-dev" do
   action :install
 end
 
 execute "install_mongrel2_python_module" do
   command "python setup.py install"
-  cwd ::File.join(n["src_dir"], "examples", "python")
-  user "root"
-  group "root"
+  cwd ::File.join(src_dir, "examples", "python")
 end
 
