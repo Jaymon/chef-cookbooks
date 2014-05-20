5fa8a915beb13326b8e012db69682d49e5e2fd5b
Jay Marcyes <jay@marcyes.com>
2014-05-20 16:24:53 -0700

    more bug fixes, holy crap

--- mongrel2/recipes/python.rb
+++ mongrel2/recipes/python.rb
@@ -4,14 +4,12 @@ n = node[name]
 include_recipe "zeromq::python"
 include_recipe "#{name}::src"
 
-src_dir = ::File.join(::Chef::Config[:file_cache_path], name, n["version"])
-
 package "python-dev" do
   action :install
 end
 
 execute "install_mongrel2_python_module" do
   command "python setup.py install"
-  cwd ::File.join(src_dir, "examples", "python")
+  cwd ::File.join(n['src_dir'], "examples", "python")
 end
 
--- mongrel2/recipes/src.rb
+++ mongrel2/recipes/src.rb
@@ -4,6 +4,7 @@ n = node[name]
 #src_version = (n["version"] != "master") ? "release/v#{n["version"]}" : n["version"]
 
 src_dir = ::File.join(::Chef::Config[:file_cache_path], name, n["version"])
+node.force_override[name]['src_dir'] = src_dir
 
 git src_dir do
   repository n["src_repo"]
