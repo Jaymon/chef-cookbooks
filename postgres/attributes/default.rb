4c9994c135a26223f8642670157242d1f816b9fb
Jay Marcyes <jay@marcyes.com>
2014-09-11 18:36:56 -0700

    lots of changes to pgbouncer recipe, but it does not work right now, changes test environment to use current working branch

--- postgres/attributes/default.rb
+++ postgres/attributes/default.rb
@@ -32,3 +32,37 @@ default[name]["hba"] = [
     'method' => 'md5',
   },
 ]
+
+# pgbouncer specific configuration
+default[name]["pgbouncer"] = {}
+default[name]["pgbouncer"]["src_dir"] = ::File.join(::Chef::Config[:file_cache_path], name, "pgbouncer")
+default[name]["pgbouncer"]["src_repo"] = "https://github.com/markokr/pgbouncer-dev.git"
+default[name]["pgbouncer"]["version"] = "1.5.4"
+
+default[name]["pgbouncer"]["databases"] = {
+  '*' => "host=127.0.0.1 port=5432", # fallback connection string
+}
+
+default[name]["pgbouncer"]["pgbouncer"] = {
+  'logfile' => ::File.join("", "var", "log", "pgbouncer", "pgbouncer.log"),
+  'pidfile' => ::File.join("", "var", "run", "pgbouncer", "pgbouncer.pid"),
+  'unix_socket_dir' = ::File.join("", "var", "run", "pgbouncer"),
+  'auth_file' => ::File.join("", "etc", "pgbouncer", "userlist.txt"),
+  "listen_addr" => "127.0.0.1", # "*" might be better default
+  "listen_port" => 6432,
+  "pool_mode" => "session",
+  "max_client_conn" => 100,
+  "default_pool_size" => 20,
+  "log_connections" => 1, 
+  "log_disconnections" => 1, 
+  "log_pooler_errors" => 1,
+  "server_check_delay" => 30,
+  "server_lifetime" => 3600,
+  "server_idle_timeout" => 600,
+  "client_login_timeout" => 60,
+  "listen_backlog" => 200
+  #"server_reset_query" => "DISCARD ALL",
+  #"auth_type" => "md5",
+}
+
+
--- postgres/recipes/pgbouncer.rb
+++ postgres/recipes/pgbouncer.rb
@@ -3,18 +3,17 @@
 #
 # since 6-14-12
 ##
+name = cookbook_name.to_s
+rname = recipe_name.to_s
+n_pg = node[name]
+n = n_pg[rname]
+n = node[name].to_hash # we need the values to be mutable
 
 # need this to get the temp directory using Dir.tmpdir
 require "tmpdir"
 
-# set some sensible defaults
-node[:postgres] ||= {}
-
 # configuration that will make its way to /etc/pgbouncer/pgbouncer.ini
 # http://pgbouncer.projects.postgresql.org/doc/config.html
-node[:postgres][:pgbouncer] ||= {}
-node[:postgres][:pgbouncer][:databases] ||= {}
-node[:postgres][:pgbouncer][:pgbouncer] ||= {}
 
 # set default databases from the top level postgres node...
 if node[:postgres].has_key?(:databases)
@@ -37,105 +36,94 @@ if node[:postgres].has_key?(:databases)
   end  
 
 end
-
-{
-  "listen_addr" => "127.0.0.1", # "*" might be better default
-  "listen_port" => 6432,
-  "pool_mode" => "transaction",
-  "server_reset_query" => "DISCARD ALL",
-  "max_client_conn" => 100,
-  "default_pool_size" => 20,
-  "log_connections" => 1, 
-  "log_disconnections" => 1, 
-  "log_pooler_errors" => 1,
-  "server_check_delay" => 30,
-  "server_lifetime" => 3600,
-  "server_idle_timeout" => 600,
-  "client_login_timeout" => 60,
-  "auth_type" => "md5",
-  "listen_backlog" => 200
-}.each do |key, val| 
-
-  if !node[:postgres][:pgbouncer][:pgbouncer].has_key?(key)
-    node[:postgres][:pgbouncer][:pgbouncer][key] = val
-  end
-
+%W{git make autoconf automake autoconf-archive asciidoc xmlto libtool libevent-dev}.each do
+  package p
 end
 
-case node[:platform]
-  
-  when "debian", "ubuntu"
-    
-    package "pgbouncer" do
-      action :upgrade
-    end
-    
-    # http://wiki.opscode.com/display/chef/Resources#Resources-Service
-    service "pgbouncer" do
-      service_name "pgbouncer"
-      supports :restart => true, :reload => false
-      action :enable
-    end
-    
-    template "/etc/pgbouncer/userlist.txt" do
-      source "pgbouncer/userlist.erb"
-      owner "postgres"
-      group "postgres"
-      mode "0640"
-      notifies :restart, resources(:service => "pgbouncer"), :delayed
-    end
-    
-    # backup the original config, just in case?
-    execute "cp /etc/pgbouncer/pgbouncer.ini /etc/pgbouncer/pgbouncer.ini.bak" do
-      user "root"
-      action :run
-      not_if "test -f /etc/pgbouncer/pgbouncer.ini.bak"
-    end
-    
-    template "/etc/pgbouncer/pgbouncer.ini" do
-      source "pgbouncer/pgbouncer.erb"
-      owner "postgres"
-      group "postgres"
-      mode "0640"
-      notifies :restart, resources(:service => "pgbouncer"), :delayed
-    end
-    
-    # patch the init.d to do what it should (this is fixed in pgbouncer 1.5.2)
-    # see: https://bugs.launchpad.net/ubuntu/+source/pgbouncer/+bug/760508
-    init_diff = "/#{Dir.tmpdir}/init.diff"
-    init_orig = "/etc/init.d/pgbouncer"
-    init_bak = "/etc/init.d/pgbouncer.bak"
-    
-    # backup original, we give it a temp name so we can apply the diff, then rename the tmp
-    # file to the actual backup name so the diff won't ever run again
-    execute "cp #{init_orig} #{init_bak}2" do
-      user "root"
-      action :run
-      not_if "test -f #{init_bak}"
-    end
-    
-    cookbook_file init_diff do
-      backup false
-      source "pgbouncer/init.diff.sh"
-      owner "root"
-    end
-    
-    execute "patch #{init_orig} < #{init_diff}" do
-      user "root"
-      action :run
-      not_if "test -f #{init_bak}"
-    end
-    
-    execute "mv #{init_bak}2 #{init_bak}" do
-      user "root"
-      action :run
-      not_if "test -f #{init_bak}"
-    end
-    
-    execute "activate pgbouncer" do
-      user "root"
-      action :run
-      command "cd /etc/default; sed 's/START=0/START=1/' pgbouncer > pgbouncer2; mv pgbouncer2 pgbouncer"
-    end
-    
+
+branch = "pgbouncer_#{n['version'].gsub('.', '_')}"
+git n['src_dir'] do
+  repository n["src_repo"]
+  revision branch
+  action :sync
+  depth 1
+  enable_submodules true
+  notifies :run, "execute[configure pgbouncer]", :immediately
 end
+
+execute "configure pgbouncer" do
+  command ""
+  cwd n['src_dir']
+
+
+
+
+
+# http://wiki.opscode.com/display/chef/Resources#Resources-Service
+service "pgbouncer" do
+  service_name "pgbouncer"
+  supports :restart => true, :reload => false
+  action :enable
+end
+
+template "/etc/pgbouncer/userlist.txt" do
+  source "pgbouncer/userlist.erb"
+  owner "postgres"
+  group "postgres"
+  mode "0640"
+  notifies :restart, resources(:service => "pgbouncer"), :delayed
+end
+
+# backup the original config, just in case?
+execute "cp /etc/pgbouncer/pgbouncer.ini /etc/pgbouncer/pgbouncer.ini.bak" do
+  user "root"
+  action :run
+  not_if "test -f /etc/pgbouncer/pgbouncer.ini.bak"
+end
+
+template "/etc/pgbouncer/pgbouncer.ini" do
+  source "pgbouncer/pgbouncer.erb"
+  owner "postgres"
+  group "postgres"
+  mode "0640"
+  notifies :restart, resources(:service => "pgbouncer"), :delayed
+end
+
+# patch the init.d to do what it should (this is fixed in pgbouncer 1.5.2)
+# see: https://bugs.launchpad.net/ubuntu/+source/pgbouncer/+bug/760508
+init_diff = "/#{Dir.tmpdir}/init.diff"
+init_orig = "/etc/init.d/pgbouncer"
+init_bak = "/etc/init.d/pgbouncer.bak"
+
+# backup original, we give it a temp name so we can apply the diff, then rename the tmp
+# file to the actual backup name so the diff won't ever run again
+execute "cp #{init_orig} #{init_bak}2" do
+  user "root"
+  action :run
+  not_if "test -f #{init_bak}"
+end
+
+cookbook_file init_diff do
+  backup false
+  source "pgbouncer/init.diff.sh"
+  owner "root"
+end
+
+execute "patch #{init_orig} < #{init_diff}" do
+  user "root"
+  action :run
+  not_if "test -f #{init_bak}"
+end
+
+execute "mv #{init_bak}2 #{init_bak}" do
+  user "root"
+  action :run
+  not_if "test -f #{init_bak}"
+end
+
+execute "activate pgbouncer" do
+  user "root"
+  action :run
+  command "cd /etc/default; sed 's/START=0/START=1/' pgbouncer > pgbouncer2; mv pgbouncer2 pgbouncer"
+end
+
