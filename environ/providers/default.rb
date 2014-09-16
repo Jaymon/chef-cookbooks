55ff29e407db11573c0edb4cb27af7c2cc831111
Jay Marcyes <jay@marcyes.com>
2014-09-16 02:28:45 -0700

    removes sentry configuration and an old app recipe. Modifies prod to get its server whitelist from the environment servers.sh file, makes the envrionment getter/setter from environ more generic and tolerant of input. Adds pgbouncer configuration to prod

--- environ/providers/default.rb
+++ environ/providers/default.rb
@@ -4,97 +4,9 @@ def whyrun_supported?
   true
 end
 
-class Environ
-  attr_accessor :hash, :file
+require ::File.join(::File.dirname(__FILE__), "..", "support", "environ")
 
-  def initialize(file, error_on_missing=true)
-    @hash = {}
-    @hash_changed = false
-    @file_loaded = false
-    @file = file
-    if error_on_missing
-      if !::File.file?(file)
-        raise ::Errno::ENOENT.new("#{file} does not exist")
-      end
-
-    else
-      @file_loaded = true
-    end
-  end
-
-  def read_file?()
-    return @file_loaded
-  end
-
-  def read_file()
-    if self.read_file?; return end
-
-    @hash = {}
-    @hash_changed = false
-
-    ::IO.foreach(@file) do |line|
-      # we only want environment KEY=val lines, ignore comments and/or whitespace
-      if line.match(/^[a-z0-9_]+=/i)
-        # line matches: ENV_NAME=...
-        key, val = line.split('=', 2)
-        #p "load #{key} = #{val}"
-        @hash[key.strip()] = val.strip()
-
-      elsif line.match(/^export\s+[a-z0-9_]+=/i)
-        # line matches: export ENV_NAME=...
-        discarded, env_segment = line.split(/\s+/, 2) 
-        key, val = env_segment.split('=', 2)
-        @hash[key.strip()] = val.strip()
-
-      end
-
-    end
-
-    @file_loaded = true
-
-  end
-
-  def write_file()
-    ::File.open(@file, 'w') do |f|
-      @hash.each do |key, val|
-        f.puts "#{key}=#{val}"
-      end
-    end
-
-  end
-
-  def get(key, default_val=nil)
-    self.read_file()
-    val = default_val
-    if @hash.has_key?(key)
-      val = @hash[key]
-    end
-
-    return val
-
-  end
-
-  def set(key, val)
-    self.read_file()
-    if @hash.has_key?(key) and @hash[key] == val
-      return false
-    end
-
-    @hash[key] = val
-    @hash_changed = true
-    return true
-
-  end
-
-  def merge(hash)
-    self.read_file()
-    @hash = @hash.merge(hash)
-    return true
-  end
-
-end
-
-e = Environ.new(::File.join("", "etc", "profile.d", "environ.sh"), false)
+e = EnvironVars.new(::File.join("", "etc", "profile.d", "environ.sh"), false)
 
 action :set do
   env_name = new_resource.name
@@ -128,13 +40,17 @@ action :file do
     file_name = new_resource.value
   end
 
-  e_new = Environ.new(file_name)
-  e_new.read_file
+  e_new = EnvironVars.new(file_name)
+  e_new.read_file()
 
   if e.merge(e_new.hash)
 
     e.hash.each do |k, v|
-      ENV[k] = v # keep the RUBY env in sync
+      # keep the RUBY env in sync, this isn't bulletproof since file might include
+      # strings (export FOO='string') or variable expansion (eg, export FOO=$BAR)
+      # better solution might be to run the file in bash and get all the values after
+      # expansion and load them in
+      ENV[k] = v 
     end
 
     converge_by("merge #{file_name} into #{e.file}") do
---
+++ environ/support/environ.rb
@@ -0,0 +1,118 @@
+
+class EnvironVars
+  attr_accessor :hash, :file
+
+  def initialize(file, error_on_missing=true)
+    @hash = {}
+    @hash_changed = false # I don't think I do anything with this anymore
+    @file_loaded = false
+    @file = file
+    if error_on_missing
+      if !::File.file?(file)
+        raise ::Errno::ENOENT.new("#{file} does not exist")
+      end
+
+    else
+      @file_loaded = true
+    end
+  end
+
+  def empty?()
+    return @hash.empty?
+  end
+
+  def read_file?()
+    return @file_loaded
+  end
+
+  def read_file()
+    if self.read_file?; return end
+
+    @hash = {}
+    @hash_changed = false
+
+    ::IO.foreach(@file) do |line|
+      # we only want environment KEY=val lines, ignore comments and/or whitespace
+      key = ""
+      if line.match(/^[a-z0-9_]+=/i)
+        # line matches: ENV_NAME=...
+        key, val = line.split('=', 2)
+        #p "load #{key} = #{val}"
+        #@hash[key.strip()] = val.strip()
+
+      elsif line.match(/^export\s+[a-z0-9_]+=/i)
+        # line matches: export ENV_NAME=...
+        discarded, env_segment = line.split(/\s+/, 2) 
+        key, val = env_segment.split('=', 2)
+
+      end
+
+      key.strip!
+      if key != ""
+        # we have to use . here because "sh" doesn't have source, you can see it
+        # is using shell by running `echo $0`
+        val = `. #{@file} && echo $#{key}`.chomp
+        if val != ""
+          @hash[key] = val
+        end
+
+      end
+
+    end
+
+    @file_loaded = true
+
+  end
+
+  def write_file()
+    ::File.open(@file, 'w') do |f|
+      @hash.each do |key, val|
+        f.puts "#{key}=#{val}"
+      end
+    end
+
+  end
+
+  def get(key, default_val=nil)
+    self.read_file()
+    val = default_val
+    if @hash.has_key?(key)
+      val = @hash[key]
+    end
+
+    return val
+
+  end
+
+  def set(key, val)
+    self.read_file()
+    if @hash.has_key?(key) and @hash[key] == val
+      return false
+    end
+
+    @hash[key] = val
+    @hash_changed = true
+    return true
+
+  end
+
+  def merge(hash)
+    self.read_file()
+    @hash = @hash.merge(hash)
+    return true
+  end
+
+  def each()
+    @hash.each { |k, v| yield k, v }
+  end
+
+  def values()
+    @hash.values
+  end
+
+  def keys()
+    @hash.keys
+  end
+
+end
+
