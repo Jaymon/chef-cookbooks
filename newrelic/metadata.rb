b2daeb4619478b043e568fb1f4409fcabfd149ee
Jay Marcyes <jay@marcyes.com>
2016-02-10 19:08:41 -0800

    adds newrelic python app support and the configuration that goes with it

--- newrelic/metadata.rb
+++ newrelic/metadata.rb
@@ -5,7 +5,9 @@ maintainer_email  "jay@marcyes.com"
 description       "newrelic stuff"
 version           "0.1"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-supports          "ubuntu", "12.04"
+supports          "ubuntu", "14.04"
+depends           "pip"
 
 recipe            "server", "add newrelic server monitoring"
+recipe            "python", "adds python newrelic-agent support"
 
