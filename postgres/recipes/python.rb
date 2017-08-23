a762623f9bbfffcd103877f53f901849b1c7c515
Jay Marcyes <jay@marcyes.com>
2017-08-23 15:02:25 -0600

    updates postgres::python recipe to address a bug when installing psycopg with other pip install values, I'm actually surprised this hasn't been a problem before

--- postgres/recipes/python.rb
+++ postgres/recipes/python.rb
@@ -4,7 +4,6 @@
 # http://stackoverflow.com/questions/5420789/how-to-install-psycopg2-with-pip-on-python
 ##
 name = cookbook_name.to_s
-include_recipe "pip" # to make this work, you need depends "pip" in metadata
 
 # prerequisites
 ["libpq-dev", "python-dev"].each do |p|
@@ -13,6 +12,8 @@ include_recipe "pip" # to make this work, you need depends "pip" in metadata
     action :install
   end
 end
+
+include_recipe "pip" # to make this work, you need depends "pip" in metadata
 
 pip "psycopg2" do
   action :install
