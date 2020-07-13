57f5b1d2edadf88ab1420b4b6ed0cfcb6dfebd46
Jay Marcyes <jay@marcyes.com>
2020-07-13 12:49:34 -0600

    disables python2 update

--- update/recipes/default.rb
+++ update/recipes/default.rb
@@ -5,5 +5,5 @@ name = cookbook_name.to_s
 include_recipe "#{name}::bash"
 include_recipe "#{name}::openssl"
 include_recipe "#{name}::linux"
-include_recipe "#{name}::python2"
+#include_recipe "#{name}::python2"
 
