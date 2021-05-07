66002e52568dc96f276e61e403439d24283d9e72
Jay Marcyes <jay@marcyes.com>
2021-05-07 16:41:41 -0600

    Ugh, I'm not sure what is up with pyenv all of a sudden. This switches the order of the two commands

--- pyenv/attributes/default.rb
+++ pyenv/attributes/default.rb
@@ -6,8 +6,8 @@ default[name]["versions"] = {} # username keys with a version array
 default[name]["dir"] = ::File.join("", "opt", "pyenv")
 default[name]["repo"] = "https://github.com/pyenv/pyenv.git"
 default[name]["bash"] = [
-  #"eval \"$(pyenv init -)\"",
   "eval \"$(pyenv init --path)\"",
+  "eval \"$(pyenv init -)\"",
 ]
 default[name]["plugins"] = {
   "pyenv-virtualenv" => { # https://github.com/pyenv/pyenv-virtualenv
